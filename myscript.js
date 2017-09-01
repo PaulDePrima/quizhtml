b279d4ccdc14cacde2d11e146bff3ee3bbbb245f
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-09-01 09:58:51 -0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("#p3").click(function() {
+	$("#imagetoggler").show();
+	$("#QA").show();
+	$("#QI").show();
+	$("img").css("border", "25px solid green")
+	});
+});
+
+$(document).ready(function() {
+	$("#QA").click(function() {
+	$("#AQ").show();
+	$("#Q2").show();
+	$("#QA2").show();
+	$("#QI2").show();
+	$("img").css("border", "25px solid green")
+	});
+});
