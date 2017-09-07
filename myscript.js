2e1b9da5f0d5de7a9d23b4fb025dabc62387aa4b
PaulDePrima <31520022+PaulDePrima@users.noreply.github.com>
2017-09-07 09:45:11 -0700

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
 	$("#p3").click(function() {
-	$("#imagetoggler").show();
+	$("#Q1").show();
 	$("#QA").show();
 	$("#QI").show();
-	$("img").css("border", "25px solid green")
+	$("#pic").animate({left: '+=500px'},"slow");
+	//$("#pic").animate({top: '+=125px'},"slow");
+	$("#pic").animate({left: '-=500px'},"slow");
+	//$("#pic").animate({top: '+=125px'},"slow");
+	//$("#pic").animate({left: '+=125px'},"slow");
+	//$("#pic").animate({top: '-=125px'},"slow");
+	//$("#pic").animate({left: '-=125px'},"slow");
+	//$("#pic").animate({top: '-=125px'},"slow");
+	$("img").css("border", "25px solid green");
+	});
+	$("#pic").click(function() {
+		$("#pic").animate({left: '+=500px'},"slow");
+	//$("#pic").animate({top: '+=125px'},"slow");
+	$("#pic").animate({left: '-=500px'},"slow");
+	//$("#pic").animate({top: '+=125px'},"slow");
+	//$("#pic").animate({left: '+=125px'},"slow");
+	//$("#pic").animate({top: '-=125px'},"slow");
+	//$("#pic").animate({left: '-=125px'},"slow");
+	//$("#pic").animate({top: '-=125px'},"slow");
 	});
 });
 
@@ -13,6 +31,49 @@ $(document).ready(function() {
 	$("#Q2").show();
 	$("#QA2").show();
 	$("#QI2").show();
-	$("img").css("border", "25px solid green")
+	$("img").css("border", "25px solid green");
+	});
+});
+
+$(document).ready(function() {
+	$("#QI").click(function() {
+	$("#IQ").show();
+	$("#Q2").show();
+	$("#QA2").show();
+	$("#QI2").show();
+	$("img").css("border", "25px solid green");
+	});
+});
+
+$(document).ready(function() {
+	$("#QA2").click(function() {
+	$("#AQ2").show();
+	$("#Q3").show();
+	$("#QA3").show();
+	$("#QI3").show();
+	$("img").css("border", "25px solid green");
+	});
+});
+
+$(document).ready(function() {
+	$("#QI2").click(function() {
+	$("#IQ2").show();
+	$("#Q3").show();
+	$("#QA3").show();
+	$("#QI3").show();
+	$("img").css("border", "25px solid green");
+	});
+});
+
+
+$(document).ready(function() {
+	$("#QA3").click(function() {
+	$("#AQ3").show();
+	});
+});
+
+$(document).ready(function() {
+	$("#QI3").click(function() {
+	$("#IQ3").show();
 	});
 });
